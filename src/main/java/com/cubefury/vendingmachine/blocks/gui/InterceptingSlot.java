803d62cfb5305c32f3d0160ed2d7464f96ff00ad
cubefury <thetarragondevteam@gmail.com>
2025-10-18 12:38:56 +0800

    Disabled coin insert/eject when vm is not formed

--- src/main/java/com/cubefury/vendingmachine/blocks/gui/InterceptingSlot.java
+++ src/main/java/com/cubefury/vendingmachine/blocks/gui/InterceptingSlot.java
@@ -5,18 +5,25 @@ import net.minecraft.item.ItemStack;
 
 import com.cleanroommc.modularui.utils.item.ItemStackHandler;
 import com.cleanroommc.modularui.widgets.slot.ModularSlot;
+import com.cubefury.vendingmachine.blocks.MTEVendingMachine;
 import com.cubefury.vendingmachine.storage.NameCache;
 import com.cubefury.vendingmachine.trade.CurrencyItem;
 import com.cubefury.vendingmachine.trade.TradeManager;
 
 public class InterceptingSlot extends ModularSlot {
 
-    public InterceptingSlot(ItemStackHandler inputItems, int index) {
+    private MTEVendingMachine vm;
+
+    public InterceptingSlot(ItemStackHandler inputItems, int index, MTEVendingMachine vm) {
         super(inputItems, index);
+        this.vm = vm;
     }
 
     // intercept item on both ends, but only do the post-intercept actions on server side
     public boolean intercept(ItemStack newItem, boolean client, EntityPlayer player) {
+        if (vm == null || !vm.getActive()) {
+            return false;
+        }
         CurrencyItem mapped = mapToCurrency(newItem);
         if (mapped != null) {
             this.putStack(null);
--- src/main/java/com/cubefury/vendingmachine/blocks/gui/MTEVendingMachineGui.java
+++ src/main/java/com/cubefury/vendingmachine/blocks/gui/MTEVendingMachineGui.java
@@ -287,7 +287,7 @@ public class MTEVendingMachineGui extends MTEMultiBlockBaseGui {
 
     // Eject code is in GUI instead of MTE since the syncers are per-gui instance
     private void doEjectCoin(CurrencyType type) {
-        if (this.guiData.isClient()) {
+        if (this.guiData.isClient() || this.base == null || !this.base.getActive()) {
             return;
         }
         UUID currentUser = NameCache.INSTANCE.getUUIDFromPlayer(base.getCurrentUser());
@@ -314,6 +314,11 @@ public class MTEVendingMachineGui extends MTEMultiBlockBaseGui {
             return;
         }
 
+        if (!this.base.getActive()) {
+            ejectCoins = false;
+            return;
+        }
+
         UUID currentUser = NameCache.INSTANCE.getUUIDFromPlayer(base.getCurrentUser());
         if (!TradeManager.INSTANCE.playerCurrency.containsKey(currentUser)) {
             ejectCoins = false;
@@ -335,6 +340,11 @@ public class MTEVendingMachineGui extends MTEMultiBlockBaseGui {
         if (this.guiData.isClient()) {
             return;
         }
+        if (!this.base.getActive()) {
+            ejectItems = false;
+            return;
+        }
+
         for (int i = 0; i < MTEVendingMachine.INPUT_SLOTS; i++) {
             ItemStack stack = base.inputItems.getStackInSlot(i);
             if (stack != null) {
@@ -395,7 +405,7 @@ public class MTEVendingMachineGui extends MTEMultiBlockBaseGui {
         return SlotGroupWidget.builder()
             .matrix("II", "II", "II")
             .key('I', index -> {
-                InterceptingSlot slot = new InterceptingSlot(base.inputItems, index);
+                InterceptingSlot slot = new InterceptingSlot(base.inputItems, index, this.base);
                 this.inputSlots.add(slot);
                 return new ItemSlot().slot(
                     slot.slotGroup("inputSlotGroup")
