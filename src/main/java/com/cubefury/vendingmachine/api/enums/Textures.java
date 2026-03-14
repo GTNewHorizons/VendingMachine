d6a076c577ceac34ba14608e6e8d8100e592363f
Léa Gris <lea.gris@noiraude.net>
2026-03-14 19:09:01 +0100

    Impr(Textures): Adapt GT5U OptionalResource (#74)

--- src/main/java/com/cubefury/vendingmachine/api/enums/Textures.java
+++ src/main/java/com/cubefury/vendingmachine/api/enums/Textures.java
@@ -1,27 +1,28 @@
 package com.cubefury.vendingmachine.api.enums;
 
-import gregtech.api.enums.Textures.BlockIcons.CustomIcon;
+import gregtech.api.enums.Textures.BlockIcons;
 import gregtech.api.interfaces.IIconContainer;
 
 public class Textures {
 
-    public static final CustomIcon VM_MACHINE_FRONT_OFF = new CustomIcon("vendingmachine:vending_machine_front_off"),
-        VM_MACHINE_FRONT_ON = new CustomIcon("vendingmachine:vending_machine_front_on"),
-        VM_MACHINE_FRONT_ON_GLOW = new CustomIcon("vendingmachine:vending_machine_front_on_glow"),
+    public static final IIconContainer VM_MACHINE_FRONT_OFF = BlockIcons
+        .custom("vendingmachine:vending_machine_front_off"),
+        VM_MACHINE_FRONT_ON = BlockIcons.custom("vendingmachine:vending_machine_front_on"),
+        VM_MACHINE_FRONT_ON_GLOW = BlockIcons.custom("vendingmachine:vending_machine_front_on_glow"),
 
-        VM_OVERLAY_0 = new CustomIcon("vendingmachine:vending_machine_overlay_0"),
-        VM_OVERLAY_1 = new CustomIcon("vendingmachine:vending_machine_overlay_1"),
-        VM_OVERLAY_2 = new CustomIcon("vendingmachine:vending_machine_overlay_2"),
-        VM_OVERLAY_3 = new CustomIcon("vendingmachine:vending_machine_overlay_3"),
-        VM_OVERLAY_4 = new CustomIcon("vendingmachine:vending_machine_overlay_4"),
+        VM_OVERLAY_0 = BlockIcons.custom("vendingmachine:vending_machine_overlay_0"),
+        VM_OVERLAY_1 = BlockIcons.custom("vendingmachine:vending_machine_overlay_1"),
+        VM_OVERLAY_2 = BlockIcons.custom("vendingmachine:vending_machine_overlay_2"),
+        VM_OVERLAY_3 = BlockIcons.custom("vendingmachine:vending_machine_overlay_3"),
+        VM_OVERLAY_4 = BlockIcons.custom("vendingmachine:vending_machine_overlay_4"),
 
-        VM_OVERLAY_ACTIVE_0 = new CustomIcon("vendingmachine:vending_machine_overlay_active_0"),
-        VM_OVERLAY_ACTIVE_1 = new CustomIcon("vendingmachine:vending_machine_overlay_active_1"),
-        VM_OVERLAY_ACTIVE_2 = new CustomIcon("vendingmachine:vending_machine_overlay_active_2"),
-        VM_OVERLAY_ACTIVE_3 = new CustomIcon("vendingmachine:vending_machine_overlay_active_3"),
+        VM_OVERLAY_ACTIVE_0 = BlockIcons.custom("vendingmachine:vending_machine_overlay_active_0"),
+        VM_OVERLAY_ACTIVE_1 = BlockIcons.custom("vendingmachine:vending_machine_overlay_active_1"),
+        VM_OVERLAY_ACTIVE_2 = BlockIcons.custom("vendingmachine:vending_machine_overlay_active_2"),
+        VM_OVERLAY_ACTIVE_3 = BlockIcons.custom("vendingmachine:vending_machine_overlay_active_3"),
 
-        VUPLINK_OVERLAY_INACTIVE = new CustomIcon("vendingmachine:vending_uplink_machine_overlay_inactive"),
-        VUPLINK_OVERLAY_ACTIVE = new CustomIcon("vendingmachine:vending_uplink_machine_overlay_active");
+        VUPLINK_OVERLAY_INACTIVE = BlockIcons.custom("vendingmachine:vending_uplink_machine_overlay_inactive"),
+        VUPLINK_OVERLAY_ACTIVE = BlockIcons.custom("vendingmachine:vending_uplink_machine_overlay_active");
 
     public static final IIconContainer[] VM_OVERLAY_ACTIVE = { VM_OVERLAY_ACTIVE_0, VM_OVERLAY_ACTIVE_1,
         VM_OVERLAY_ACTIVE_2, VM_OVERLAY_ACTIVE_3, VM_OVERLAY_4 // bottom right not animated
