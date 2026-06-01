615352fe0bc891c83938d018ad4fccacda2aaf41
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-06-02 00:14:42 +0200

    spelling

--- src/main/java/com/cubefury/vendingmachine/util/ColorUtils.java
+++ src/main/java/com/cubefury/vendingmachine/util/ColorUtils.java
@@ -8,17 +8,17 @@ public class ColorUtils {
 
     public static final ColorResource
     // spotless:off
-        displayText                     = color.rgb("displayText",                      "#000000"),
-        textColorConditionDefault       = color.rgb("textColorConditionDefault",        "#000000"),
-        textColorConditionSatisfied     = color.rgb("textColorConditionSatisfied",      "#55D441"),
-        textColorConditionUnsatisfied   = color.rgb("textColorConditionUnsatisfied",    "#A87A5E"),
-        nc_InputsOverlay                = color.rgb("nc_InputsOverlay",                 "#FDD835"),
-        tradeDisplayText                = color.rgb("tradeDisplayText",                 "#FFFFFF"),
+        displayText                     = color.rgb("displayText",                      "0x000000"),
+        textColorConditionDefault       = color.rgb("textColorConditionDefault",        "0x000000"),
+        textColorConditionSatisfied     = color.rgb("textColorConditionSatisfied",      "0x55D441"),
+        textColorConditionUnsatisfied   = color.rgb("textColorConditionUnsatisfied",    "0xA87A5E"),
+        nc_InputsOverlay                = color.rgb("nc_InputsOverlay",                 "0xFDD835"),
+        tradeDisplayText                = color.rgb("tradeDisplayText",                 "0xFFFFFF"),
 
-        tradeDisplayDisabled            = color.argb("tradeDisplayDisabled",            "#BB000000"),
-        tradeDisplayListTradableNow     = color.argb("tradeDisplayListTradableNow",     "#883CFF00"),
-        tradeDisplayListUntradableNow   = color.argb("tradeDisplayListUntradableNow",   "#88333333"),
-        tradeDisplayListCurrentSelected = color.argb("tradeDisplayListCurrentSelected", "#AA039BE5"),
-        volumeSliderBackground          = color.argb("volumeSliderBackground",          "#FF000000");
+        tradeDisplayDisabled            = color.argb("tradeDisplayDisabled",            "0xBB000000"),
+        tradeDisplayListTradableNow     = color.argb("tradeDisplayListTradableNow",     "0x883CFF00"),
+        tradeDisplayListUntradableNow   = color.argb("tradeDisplayListUntradableNow",   "0x88333333"),
+        tradeDisplayListCurrentSelected = color.argb("tradeDisplayListCurrentSelected", "0xAA039BE5"),
+        volumeSliderBackground          = color.argb("volumeSliderBackground",          "0xFF000000");
     // spotless:on
 }
